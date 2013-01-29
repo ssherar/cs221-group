8062579a475fe01ce03e037ab8d42a5b65dd6642
Sam Sherar <s.sherar@live.com>
2013-01-29 20:04:42 +0000

    Removing redundant code

--- n15/src/MonsterMash/src/uk/ac/aber/dcs/cs221/n15/Controller/MonsterDAO.java
+++ n15/src/MonsterMash/src/uk/ac/aber/dcs/cs221/n15/Controller/MonsterDAO.java
@@ -90,54 +90,6 @@ public class MonsterDAO {
 		}
 	}
 	
-//	public void age(Monster monster) {
-//		Date now = new Date();
-//		Random r = new Random();
-//		int strength, aggression, fertility, health, scalar;
-//		long ageMilliseconds;
-//		boolean edited = false;
-//		//TypedQuery<Monster> q = (TypedQuery<Monster>) emf.createEntityManager().createNativeQuery("SELECT * FROM monsters WHERE id = '"+monsterID+"' LIMIT 1", Monster.class);
-//		//Monster monster = q.getSingleResult();
-//		
-//		ageMilliseconds = now.getTime() - monster.getDob().getTime();
-//		
-//		if(ageMilliseconds < 432000000) { //TODO: Fix without milliseconds
-//			scalar = (int) (ageMilliseconds / 20000000);
-//			strength = r.nextInt() * scalar;
-//			aggression = r.nextInt() * scalar;
-//			fertility = r.nextInt() * scalar;
-//			
-//			monster.setStrength(monster.getStrength() + strength);
-//			monster.setAggression(monster.getAggression() + aggression);
-//			monster.setFertility(monster.getFertility() + fertility);
-//			edited = true;
-//			
-//		} else if(ageMilliseconds > 1036800000) {
-//			scalar = -4;
-//			strength = r.nextInt() * scalar;
-//			aggression = r.nextInt() * scalar;
-//			fertility = r.nextInt() * scalar;
-//			health = r.nextInt() * scalar;
-//			
-//			monster.setStrength(monster.getStrength() + strength);
-//			monster.setAggression(monster.getAggression() + aggression);
-//			monster.setFertility(monster.getFertility() + fertility);
-//			monster.setHealth(monster.getHealth() + health);
-//			edited = true;
-//		}
-//		
-//		if(edited) {
-//			try {
-//				UserTransaction transaction = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
-//				transaction.begin();
-//				em.persist(monster);
-//				transaction.commit();
-//			} catch(Exception ex) {
-//				
-//			}
-//		}
-//	}
-	
 	public void ageMonsters(List<Monster> monsters) {
 		for(Monster m : monsters) {
 			this.age(m);
