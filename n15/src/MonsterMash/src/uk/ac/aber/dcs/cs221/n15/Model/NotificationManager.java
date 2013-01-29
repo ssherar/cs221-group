82567dd7860d8dee82672fa11b5e8d74f335647c
Kamil Mrowiec <kaamilm@gmail.com>
2013-01-29 18:31:45 +0000

    Refactored NotificationManager

--- n15/src/MonsterMash/src/uk/ac/aber/dcs/cs221/n15/Model/NotificationManager.java
+++ n15/src/MonsterMash/src/uk/ac/aber/dcs/cs221/n15/Model/NotificationManager.java
@@ -16,9 +16,13 @@ import uk.ac.aber.dcs.cs221.n15.Controller.RequestType;
 public class NotificationManager {
 
 	private User user;
+	private RequestDAO rdao;
+	StringBuilder builder;
 	
 	public NotificationManager(User u){
 		this.user = u;
+		this.rdao = new RequestDAO();
+		this.builder = new StringBuilder();
 	}
 	
 	/*
@@ -26,51 +30,74 @@ public class NotificationManager {
 	 * Type specifies what type of requests are to be displayed as notifications.
 	 */
 	public String getNotifications(RequestType type){
-		StringBuilder sb = new StringBuilder();
-		RequestDAO rdao = new RequestDAO();
+		
 		List<Request> requests = rdao.getRequests(user, type);
 		
-		if(type == RequestType.FRIEND_REQUEST)
-		for(Request r : requests){
-			//If the user sent the request
-			if(r.getSourceID().equals(user.getId())){
-				sb.append("<div class=\"request_window\">");
-				sb.append("Sent invitation to ");
-				sb.append(r.getTargetID().substring(r.getTargetID().lastIndexOf(".")+1));
-				sb.append(",<br/> waiting for response.</div>");
-			}else{
-				//If the user received the request
-				sb.append("<div class=\"request_window\">");
-				sb.append(r.getSourceID().substring(r.getSourceID().lastIndexOf(".")+1));
-				sb.append(" wants to be friends with you!<br/>");
-				String acceptLink = "RequestDispatcherServlet?action=accept&type=1&requestid="+r.getId();
-				String declineLink = "RequestDispatcherServlet?action=decline&type=2&requestid="+r.getId();
-				sb.append("<a href=\""+acceptLink+"\">accept </a>");
-				sb.append("|<a href=\""+declineLink+"\"> decline </a></div>");
-				
+		switch(type){
+		case FRIEND_REQUEST:
+			for(Request r : requests)
+				builder.append(processFriendRequest(r));
+			break;
+		
+		case ACCEPTED_FRIENDSHIP:
+			for(Request r : requests){
+				if(r.getTargetID().equals(user.getId())) continue;
+				else builder.append(processAcceptedFriendship(r));
 			}
-			
-		}else if(type == RequestType.ACCEPTED_FRIENDSHIP){
+			break;
+		
+		case DECLINED_FRIENDSHIP:
 			for(Request r : requests){
 				if(r.getTargetID().equals(user.getId())) continue;
-				sb.append("<div class=\"request_window\">");
-				sb.append(r.getTargetID().substring(r.getTargetID().lastIndexOf(".")+1));
-				sb.append(" accepted your invitation.");
-				sb.append("<a href=\"RequestDispatcherServlet?action=dismiss&requestid="+r.getId()+"\">");
-				sb.append("<img src=\"img/close.png\" width=\"10px\" /></a></div>");
-			}				
-		}else if(type == RequestType.DECLINED_FRIENDSHIP){
-			for(Request r : requests){
-				if(r.getTargetID().equals(user.getId())) continue;
-				sb.append("<div class=\"request_window\">");
-				sb.append(r.getTargetID().substring(r.getTargetID().lastIndexOf(".")+1));
-				sb.append(" declined your invitation.");
-				sb.append("<a href=\"RequestDispatcherServlet?action=dismiss&requestid="+r.getId()+"\">");
-				sb.append("<img src=\"img/close.png\" width=\"10px\" /></a></div>");
-			}				
+				
+			}	
+			break;				
 		}
 		
 		
+		return builder.toString();
+	}
+	
+	private String processFriendRequest(Request r){
+		StringBuilder sb = new StringBuilder();
+		//If the user sent the request
+		if(r.getSourceID().equals(user.getId())){
+			sb.append("<div class=\"request_window\">");
+			sb.append("Sent invitation to ");
+			sb.append(r.getTargetID().substring(r.getTargetID().lastIndexOf(".")+1));
+			sb.append(",<br/> waiting for response.</div>");
+		}else{
+			//If the user received the request
+			sb.append("<div class=\"request_window\">");
+			sb.append(r.getSourceID().substring(r.getSourceID().lastIndexOf(".")+1));
+			sb.append(" wants to be friends with you!<br/>");
+			String acceptLink = "RequestDispatcherServlet?action=accept&type=1&requestid="+r.getId();
+			String declineLink = "RequestDispatcherServlet?action=decline&type=2&requestid="+r.getId();
+			sb.append("<a href=\""+acceptLink+"\">accept </a>");
+			sb.append("|<a href=\""+declineLink+"\"> decline </a></div>");
+			
+		}
+	return sb.toString();
+	}
+	
+	private String processAcceptedFriendship(Request r){
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<div class=\"request_window\">");
+		sb.append(r.getTargetID().substring(r.getTargetID().lastIndexOf(".")+1));
+		sb.append(" accepted your invitation.");
+		sb.append("<a href=\"RequestDispatcherServlet?action=dismiss&requestid="+r.getId()+"\">");
+		sb.append("<img src=\"img/close.png\" width=\"10px\" /></a></div>");
+		return sb.toString();
+	}
+	
+	private String processDeclinedFriendship(Request r){
+		StringBuilder sb = new StringBuilder();
+		sb.append("<div class=\"request_window\">");
+		sb.append(r.getTargetID().substring(r.getTargetID().lastIndexOf(".")+1));
+		sb.append(" declined your invitation.");
+		sb.append("<a href=\"RequestDispatcherServlet?action=dismiss&requestid="+r.getId()+"\">");
+		sb.append("<img src=\"img/close.png\" width=\"10px\" /></a></div>");
 		return sb.toString();
 	}
 }
